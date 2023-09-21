60e37d42e4416e0d4508b864db4a446b50f2f5df
Tim Shaw <timothyshaw0225@gmail.com>
2023-09-21 18:45:43 -0400

    add sprite gif

--- Animated-Sprite/export/neko/haxe/ApplicationMain.hx
+++ Animated-Sprite/export/neko/haxe/ApplicationMain.hx
@@ -35,7 +35,7 @@ class ApplicationMain
 		ManifestResources.init(config);
 		#end
 
-		app.meta["build"] = "327";
+		app.meta["build"] = "328";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "Tilemap";
 		app.meta["name"] = "Tilemap";
