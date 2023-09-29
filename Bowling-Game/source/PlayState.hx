6c32a99d97e096f018257522f18e3dbaacf75c2f
Tim Shaw <timothyshaw0225@gmail.com>
2023-09-29 14:05:49 -0400

    sorry

---
+++ Bowling-Game/source/PlayState.hx
@@ -0,0 +1,29 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.graphics.FlxGraphic;
+import flixel.group.FlxGroup;
+import flixel.math.FlxPoint;
+import flixel.text.FlxText;
+import flixel.tile.FlxTilemap;
+import flixel.ui.FlxButton;
+import flixel.util.FlxColor;
+import flixel.util.FlxSpriteUtil;
+import flixel.util.FlxStringUtil;
+import openfl.Assets;
+
+class PlayState extends FlxState
+{
+	override public function create():Void
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+}
---
+++ Bowling-Game/source/TitleScreen.hx
@@ -0,0 +1,39 @@
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.util.FlxColor;
+
+class TitleScreen extends FlxState
+{
+	var titleText:FlxText;
+	var play_button:FlxButton;
+	var AI_button:FlxButton;
+
+	override public function create():Void
+	{
+		super.create();
+		titleText = new FlxText(0, 0, FlxG.width / 2, "Tic\nTac\nToe");
+		titleText.setFormat(null, 64, FlxColor.ORANGE);
+		titleText.screenCenter();
+		titleText.x -= 40;
+		add(titleText);
+
+		play_button = new FlxButton(0, 0, "2 Player", play);
+		play_button.screenCenter();
+		play_button.x += 80;
+		play_button.y -= 50;
+
+		add(play_button);
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+
+	public function play()
+	{
+		FlxG.switchState(new PlayState());
+	}
+}
