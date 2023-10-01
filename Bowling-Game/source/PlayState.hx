699563142c829b934af8abde4e7fc792740437cd
Tim Shaw <timothyshaw0225@gmail.com>
2023-10-01 14:08:02 -0400

    add circle collision physics

--- Bowling-Game/source/PlayState.hx
+++ Bowling-Game/source/PlayState.hx
@@ -34,11 +34,13 @@ class PlayState extends FlxState
 		bowling_ball.loadGraphic("assets/Bowling-Ball-Spritesheet.png", true, 64, 64);
 		bowling_ball.screenCenter();
 		bowling_ball.setGraphicSize(Math.round(bowling_ball.width), Math.round(bowling_ball.height));
+		bowling_ball.x += 20;
 		bowling_ball.y += 100;
 		bowling_ball.allowCollisions = ANY;
-		bowling_ball.collisionXDrag = CollisionDragType.IMMOVABLE;
-		bowling_ball.collisionYDrag = CollisionDragType.IMMOVABLE;
+		// bowling_ball.collisionXDrag = CollisionDragType.IMMOVABLE;
+		// bowling_ball.collisionYDrag = CollisionDragType.IMMOVABLE;
 		bowling_ball.velocity.y = -100;
+		bowling_ball.velocity.x -= 10;
 		add(bowling_ball);
 
 		pins = new FlxGroup();
@@ -49,8 +51,8 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-		FlxG.collide(bowling_ball, pins);
-		FlxG.collide(pins, pins);
+		FlxG.overlap(bowling_ball, pins, separateCircle);
+		FlxG.overlap(pins, pins, separateCircle);
 		bowling_ball.velocity.y = -100;
 	}
 
@@ -70,8 +72,59 @@ class PlayState extends FlxState
 				pin.setGraphicSize(Math.round(pin.width / 1.2), Math.round(pin.height / 1.2));
 				pin.updateHitbox();
 				pin.allowCollisions = ANY;
+				pin.drag.x = 10;
+				pin.drag.y = 10;
 				pins.add(pin);
 			}
 		}
+	}
+
+	public static function separateCircle(circle1:FlxSprite, circle2:FlxSprite):Bool
+	{
+		// Determine max distance between center of circles
+		var totalRadius:Float = circle1.width / 2 + circle2.width / 2;
+		var c1 = circle1.getMidpoint(FlxPoint.weak());
+		var c2 = circle2.getMidpoint(FlxPoint.weak());
+
+		// Get squared distance between circle midpoints. We square here to avoid a slow square root.
+		var distanceSquared:Float = (c1.x - c2.x) * (c1.x - c2.x) + (c1.y - c2.y) * (c1.y - c2.y);
+
+		// Prevent "Sticking" by checking if circles are actually moving towards each other
+		var dx = circle1.x - circle2.x;
+		var dy = circle1.y - circle2.y;
+		var vx = circle2.velocity.x - circle1.velocity.x;
+		var vy = circle2.velocity.y - circle1.velocity.y;
+
+		// Positive if circles are moving towards each other
+		var dot = dx * vx + dy * vy;
+
+		// If circles overlap and are moving toward each other...
+		if (distanceSquared < totalRadius * totalRadius && dot > 0)
+		{
+			// Get normalized tangent vector
+			var tangentVector = new FlxPoint(circle2.y - circle1.y, -(circle2.x - circle1.x));
+			tangentVector = tangentVector.normalize();
+
+			// Determine relative velocity
+			var relativeVelocity = new FlxPoint(circle2.velocity.x - circle1.velocity.x, circle2.velocity.y - circle1.velocity.y);
+			var length = relativeVelocity.dot(tangentVector);
+
+			// set tangent vector to correct length for velocity calculation
+			var velocityComponentOnTangent = tangentVector * length;
+
+			// get velocity perpendicular to our tangent line
+			var velocityComponentPerpendicularToTangent = relativeVelocity - velocityComponentOnTangent;
+
+			// Apply velocity to circles
+			circle1.velocity.x += velocityComponentPerpendicularToTangent.x;
+			circle1.velocity.y += velocityComponentPerpendicularToTangent.y;
+
+			circle2.velocity.x -= velocityComponentPerpendicularToTangent.x;
+			circle2.velocity.y -= velocityComponentPerpendicularToTangent.x;
+
+			return true;
+		}
+
+		return false;
 	}
 }
